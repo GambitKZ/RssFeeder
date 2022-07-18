b3783ff9c267aba1ac6571737213898932712e63
GambitKZ <rusnigdrag@gmail.com>
2022-07-18 10:02:16 +0600

    Add main Entities

---
+++ src/Domain/Entities/FeedItemObject.cs
@@ -0,0 +1,14 @@
+﻿namespace RssFeeder.Domain.Entities;
+
+public class FeedItemObject
+{
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public Uri Link { get; set; }
+
+    public string Id { get; set; }
+
+    public DateTimeOffset UpdateTime { get; set; }
+}
---
+++ src/Domain/Entities/RssFeedObject.cs
@@ -0,0 +1,16 @@
+﻿namespace RssFeeder.Domain.Entities;
+
+public class RssFeedObject
+{
+    public string FeedTable { get; set; }
+
+    public string FeedDescription { get; set; }
+
+    public Uri FeedUrl { get; set; }
+
+    public string FeedAuthor { get; set; }
+
+    public string[] FeedCategories { get; set; }
+
+    public List<FeedItemObject> Items { get; } = new List<FeedItemObject>();
+}
